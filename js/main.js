c9f9b6effc89f099fe80c63cfd75d49a872a57f2
agahafiz04 <agahafiz04@gmail.com>
2023-12-01 16:16:46 +0700

    Finishing All Changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-import { importScript } from "../js/import.js";
-importScript();
+const scriptOne = document.createElement("script");
+scriptOne.src = "https://kit.fontawesome.com/a836538f8f.js";
+scriptOne.crossOrigin = "anonymous";
+scriptOne.defer = true;
+
+const scriptTwo = document.createElement("script");
+scriptTwo.src =
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js";
+scriptTwo.defer = true;
+
+document.head.append(scriptOne, scriptTwo);
 
 console.log("test");
